b9efe6f5c49a5e286d3799a875abf67737caaf3e
jncd-v <jncdev@protonmail.com>
2022-01-14 22:42:07 +0100

    added cloudinary as media libarary to config.yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-require("dotenv").config({
-  path: `.env`,
-});
+// require("dotenv").config({
+//   path: `.env`,
+// });
 
 module.exports = {
   plugins: [
@@ -24,26 +24,6 @@ module.exports = {
               icon: `src/images/favicon.png`,
             },
           },
-          {
-            resolve: "gatsby-remark-embed-video",
-            options: {
-              width: 800,
-              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              height: 400, // Optional: Overrides optional.ratio
-              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
-              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
-              loadingStrategy: "lazy", //Optional: Enable support for lazy-load offscreen iframes. Default is disabled.
-              urlOverrides: [
-                {
-                  id: "youtube",
-                  embedURL: (videoId) =>
-                    `https://www.youtube-nocookie.com/embed/${videoId}`,
-                },
-              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
-              containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
-              iframeId: false, //Optional: if true, iframe's id will be set to what is provided after 'video:' (YouTube IFrame player API requires iframe id)
-            },
-          },
         ],
       },
     },
@@ -54,19 +34,6 @@ module.exports = {
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-cloudinary`,
-      options: {
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-        apiKey: process.env.CLOUDINARY_API_KEY,
-        apiSecret: process.env.CLOUDINARY_API_SECRET,
-        resourceType: `image`,
-        tags: true,
-        context: true,
-        prefix: `source/`,
-        maxResults: 500,
       },
     },
   ],
