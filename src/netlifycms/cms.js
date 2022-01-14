c96246f795ad782862614be1f65288f6891b5b42
jncd-v <jncdev@protonmail.com>
2022-01-14 23:55:07 +0100

    cms.js add second argument ot regmedlib

--- src/netlifycms/cms.js
+++ src/netlifycms/cms.js
@@ -2,4 +2,4 @@ import CMS from "netlify-cms-app";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
 CMS.init();
-CMS.registerMediaLibrary(cloudinary);
+CMS.registerMediaLibrary("cloudinary", cloudinary);
