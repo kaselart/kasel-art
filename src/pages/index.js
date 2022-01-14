e970333c42b2961c51e1a8fb33fd9200d4d01e7f
jncd-v <jncdev@protonmail.com>
2022-01-14 08:55:00 +0100

    remove CMS from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,7 @@
 import * as React from "react";
 import Home from "./Home";
 import "../css/App.css";
-import CMS from "netlify-cms-app";
-// Initialize the CMS object
-CMS.init();
-// Now the registry is available via the CMS object.
-// CMS.registerPreviewTemplate("my-template", MyTemplate);
+
 export default function IndexPage() {
   return (
     <>
