28a96b1827aa37a7c2c06a41dc36dd19519a6db9
jncd-v <jncdev@protonmail.com>
2022-01-22 09:12:42 +0100

    rm markdown info from home to test build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,17 @@ import FeaturedItemPreview from "../components/FeaturedItemPreview";
 import "../css/App.css";
 
 export default function Home({ data }) {
-  const allItems = data.allMarkdownRemark.nodes;
+  // console.log(data.allMarkdownRemark);
+  // const allItems = data.allMarkdownRemark.nodes;
   return (
     <Layout>
       <section className="home">
         <div className="featured">
-          {allItems.map((item, i) => {
+          {/* {allItems.map((item, i) => {
             if (i < 8) {
               return <FeaturedItemPreview item={item} />;
             }
-          })}
+          })} */}
         </div>
       </section>
     </Layout>
