5e8d800613cfe81388217ce86aee5e5f39410d6f
jncd-v <jncdev@protonmail.com>
2022-01-31 21:55:14 +0100

    commented out stripe components

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,34 +1,34 @@
-import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+// import React from "react"
+// import { graphql, StaticQuery } from "gatsby"
 
-export default function Products(props) {
-    return (
-        <StaticQuery
-        query={graphql`
-            query ProductPrices {
-                prices: allStripePrice(
-                    filter: { active: { eq: true }}
-                    sort: { fields: [unit_amount] }
-                ) {
-                    edges {
-                        node {
-                            id
-                            active
-                            currenty
-                            unit_amount
-                            product {
-                                id
-                                name
-                            }
-                        }
-                    }
-                }
-            }
-        `}
-        render={({prices}) => {
-            {prices.edges.map(({node: price}) (
-                <p key={price.id}>{price.product.name}</p>
-            ))}
-        }}/>
-    )
-}
+// export default function Products(props) {
+//     return (
+//         <StaticQuery
+//         query={graphql`
+//             query ProductPrices {
+//                 prices: allStripePrice(
+//                     filter: { active: { eq: true }}
+//                     sort: { fields: [unit_amount] }
+//                 ) {
+//                     edges {
+//                         node {
+//                             id
+//                             active
+//                             currenty
+//                             unit_amount
+//                             product {
+//                                 id
+//                                 name
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         `}
+//         render={({prices}) => {
+//             {prices.edges.map(({node: price}) (
+//                 <p key={price.id}>{price.product.name}</p>
+//             ))}
+//         }}/>
+//     )
+// }
--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -1,61 +1,61 @@
-import React, { useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
+// import React, { useState } from "react";
+// import { loadStripe } from "@stripe/stripe-js";
 
-const buttonStyles = {
-  fontSize: "13px",
-  textAlign: "center",
-  color: "#000",
-  padding: "12px 60px",
-  boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
-  backgroundColor: "rgb(255, 178, 56)",
-  borderRadius: "6px",
-  letterSpacing: "1.5px",
-};
-const buttonDisabledStyles = {
-  opacity: "0.5",
-  cursor: "not-allowed",
-};
+// const buttonStyles = {
+//   fontSize: "13px",
+//   textAlign: "center",
+//   color: "#000",
+//   padding: "12px 60px",
+//   boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
+//   backgroundColor: "rgb(255, 178, 56)",
+//   borderRadius: "6px",
+//   letterSpacing: "1.5px",
+// };
+// const buttonDisabledStyles = {
+//   opacity: "0.5",
+//   cursor: "not-allowed",
+// };
 
-let stripePromise;
-const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe(
-      "pk_test_51KNIKHEfvJ1fyk2sDc2yecuVhEUD9Qi1GetltrM8b6hx0KAaAoOkVbpYgOOdFTnEOygNlkcBurpWxbXqLRrmtNQn00RwDIlPsd"
-    );
-  }
-  return stripePromise;
-};
+// let stripePromise;
+// const getStripe = () => {
+//   if (!stripePromise) {
+//     stripePromise = loadStripe(
+//       "pk_test_51KNIKHEfvJ1fyk2sDc2yecuVhEUD9Qi1GetltrM8b6hx0KAaAoOkVbpYgOOdFTnEOygNlkcBurpWxbXqLRrmtNQn00RwDIlPsd"
+//     );
+//   }
+//   return stripePromise;
+// };
 
-const Checkout = ({ priceId }) => {
-  const [loading, setLoading] = useState(false);
+// const Checkout = ({ priceId }) => {
+//   const [loading, setLoading] = useState(false);
 
-  const redirectToCheckout = async (event) => {
-    event.preventDefault();
-    setLoading(true);
+//   const redirectToCheckout = async (event) => {
+//     event.preventDefault();
+//     setLoading(true);
 
-    const stripe = await getStripe();
-    const { error } = await stripe.redirectToCheckout({
-      mode: "payment",
-      lineItems: [{ price: priceId, quantity: 1 }],
-      successUrl: "http://localhost:8000/",
-      cancelUrl: "http://localhost:8000",
-    });
-    if (error) {
-      console.warn("Error: ", error);
-      setLoading(false);
-    }
-  };
-  return (
-    <button
-      disabled={loading}
-      style={
-        loading ? { ...buttonStyles, ...buttonDisabledStyles } : buttonStyles
-      }
-      onClick={redirectToCheckout}
-    >
-      BUY
-    </button>
-  );
-};
+//     const stripe = await getStripe();
+//     const { error } = await stripe.redirectToCheckout({
+//       mode: "payment",
+//       lineItems: [{ price: priceId, quantity: 1 }],
+//       successUrl: "http://localhost:8000/",
+//       cancelUrl: "http://localhost:8000",
+//     });
+//     if (error) {
+//       console.warn("Error: ", error);
+//       setLoading(false);
+//     }
+//   };
+//   return (
+//     <button
+//       disabled={loading}
+//       style={
+//         loading ? { ...buttonStyles, ...buttonDisabledStyles } : buttonStyles
+//       }
+//       onClick={redirectToCheckout}
+//     >
+//       BUY
+//     </button>
+//   );
+// };
 
-export default Checkout;
+// export default Checkout;
